73720b95c94c9eea988718b949a1ea48538e1a68
tongjuan <vstj007@126.com>
2018-09-06 21:51:24 +0800

    update mock

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    1111
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    mounted(){
+      axios.get('/category')
+        .then(res=>{
+          console.log(res.data)
+        })
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
